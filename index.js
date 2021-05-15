44abda9888f5f9f6ed377558972c71930e260a05
Payam Abdullah <payam97abubakr@gmail.com>
2021-05-15 19:44:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your code here
+function submitData(name,email){
+
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      "name": name,
+      "email": email,
+    })
+  }).then((response)=>{
+    return response.json()
+  }).then((json)=>{
+    console.log(json);
+    const body = document.getElementsByTagName('body')[0];
+    const p = document.createElement('p')
+    p.innerHTML = `${json.id}`
+    body.append(p);
+  }).catch((message)=>{
+    const body = document.getElementsByTagName('body')[0];
+    const p = document.createElement('p')
+    p.innerHTML = message
+    body.append(p);
+  });
+
+}
+
+const r = submitData("payam","mail");
